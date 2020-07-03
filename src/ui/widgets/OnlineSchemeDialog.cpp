08a9d86d922df838a33fcd8da4eaf1639400a190
yaoshiping <yaoshiping@dianwoda.com>
2020-07-04 00:47:30 +0800

    add onlineschemedialog code template

---
+++ src/ui/widgets/OnlineSchemeDialog.cpp
@@ -0,0 +1,6 @@
+#include "OnlineSchemeDialog.hpp"
+
+OnlineSchemeDialog::OnlineSchemeDialog(QWidget *parent) : QWidget(parent)
+{
+
+}
---
+++ src/ui/widgets/OnlineSchemeDialog.hpp
@@ -0,0 +1,17 @@
+#ifndef ONLINESCHEMEDIALOG_HPP
+#define ONLINESCHEMEDIALOG_HPP
+
+#include <QWidget>
+
+class OnlineSchemeDialog : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit OnlineSchemeDialog(QWidget *parent = nullptr);
+
+signals:
+
+public slots:
+};
+
+#endif // ONLINESCHEMEDIALOG_HPP
